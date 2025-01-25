c81bb9f27bbfd36383b676a822728e95e18f8a62
MajMajstr <137317814+MajMajstr@users.noreply.github.com>
2025-01-25 20:37:33 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,135 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+
+filterSelection("all")
+
+function filterSelection(c) {
+    var x,i;
+    x = document.getElementsByClassName("nepremicnina");
+    if(c == "all") c = "";
+    for (i=0; i< x.length; i++){
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
+
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length ;i++){
+        if(arr1.indexOf(arr2[i]) == -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length ;i++){
+        while(arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i<btns.length; i++) {
+    btns[i].addEventListener("click", function(){ 
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className+= " active";
+    });
+}
+
+
+
+
+
+
+
+
+filterSelection1("all")
+
+function filterSelection1(c) {
+    var x,i;
+    x = document.getElementsByClassName("nepremicnina");
+    if(c == "all") c = "";
+    for (i=0; i< x.length; i++){
+        removeClass1(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass1(x[i], "show");
+    }
+}
+
+function addClass1(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length ;i++){
+        if(arr1.indexOf(arr2[i]) == -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass1(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length ;i++){
+        while(arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" -");
+}
+
+var btnContainer1 = document.getElementById("myBtnContainer1");
+var btns1 = btnContainer.getElementsByClassName("btn1");
+for (var i = 0; i<btns1.length; i++) {
+    btns1[i].addEventListener("click", function(){ 
+        var current = document.getElementsByClassName("active1");
+        current[0].className = current[0].className.replace(" active1", "");
+        this.className+= " active1";
+    });
+}
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
